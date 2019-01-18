2afe33ca964b88809144b1d83011fdc854e30996
Marzuk Rashid <marzukr@gmail.com>
2019-01-17 22:32:57 -0600

    register w/ error handling

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,49 +11,67 @@ const userSchema = new Schema({
         type: String,
         index: true,
         unique: true,
-        required: true,
+        required: [true, "An email is required."],
+        validate: {
+            validator: (v) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(v),
+            message: "Please enter a valid email address."
+        }
     },
     password: {
         type: String,
-        required: true,
+        required: [true, "A password is required."],
+        validate: [
+            {
+                validator: (v) => v.length >= 8 && v.length <= 64,
+                message: "The password must be between 8 and 64 characters long."
+            },
+            {
+                validator: (v) => !commonList.test(v),
+                message: "We need a more secure password. Try something memorable but unique."
+            }
+        ]
     },
     firstName: {
         type: String,
-        required: true,
+        required: [true, "A first name is required."],
     },
     lastName: {
         type: String,
-        required: true,
+        required: [true, "A last name is required."],
     }
 });
 
 //use regular function and not arrow function to work properly
 userSchema.pre("save", async function(next) {
-    //only hash password if it has been modified (or is new)
-    if(this.isModified("password")) return next();
 
-    if(this.password.length < 8 || this.password.length > 64) {
-        return next("The password must be between 8 and 64 characters long.");
-    }
-
-    if(commonList.test(this.password)) {
-        return next("This is an insecure password. Try something memorable but unique.");
-    }
-    
-    //generate a salt
-    bcrypt.genSalt(hashCost, function(err, salt) {
-        if(err) return next(err);
-
+    try {
+        //only hash password if it has been modified (or is new)
+        if(!this.isModified("password")) return next();
+        //generate a salt
+        const salt = await bcrypt.genSalt(hashCost)
         //hash with the generated salt
-        bcrypt.hash(this.password, salt, function(err, hash) {
-            if(err) return next(err);
+        const hash = await bcrypt.hash(this.password, salt);
+        //override the cleartext password with the hashed one
+        this.password = hash;
+        return next();
+    }
+    catch (e) {
+        return next(e);
+    }
+    //generate a salt
+    // bcrypt.genSalt(hashCost, function(err, salt) {
+    //     if(err) return next(err);
 
-            //override the cleartext password with the hashed one
-            this.password = hash;
-            next();
-        })
-    })
-    next();
+    //     //hash with the generated salt
+    //     bcrypt.hash(this.password, salt, function(err, hash) {
+    //         if(err) return next(err);
+
+    //         //override the cleartext password with the hashed one
+    //         this.password = hash;
+    //         next();
+    //     })
+    // })
+    // next();
 });
 
 userSchema.methods.comparePassword = function(candidatePassword, cb) {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,18 +9,25 @@ const UserModel = require('../models/user');
 const router = express.Router();
 
 router.post('/register', async (req, res) => {
-    const {email, password, passConf, firstName, lastName} = req.body;
+    const { email, password, firstName, lastName } = req.body;
 
     try {
-        const userDocument = new UserModel({
-            username: username, 
-            passwordHash: password
-        });
+        const userDocument = new UserModel({ email, password, firstName, lastName });
         await userDocument.save();
-        res.status(200).send({username});
+        return res.status(200).send({email});
     } 
     catch (error) {
-        res.status(400).send({error: error.message});
+        if(error.name === "ValidationError") {
+            let errors = {};
+            for(path in error.errors) {
+                errors[path] = error.errors[path].message;
+            }
+            return res.status(400).json(errors);
+        }
+        else if (error.name === "MongoError" && (error.code === 11000 || error.code === 11001)) {
+            return res.status(400).json({email: "That email is already used."});
+        }
+        return res.status(400).send({error: "There was an error. Please try again."});
     }
 });
 
