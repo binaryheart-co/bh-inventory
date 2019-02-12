1f8338cbcd81830539c93928de0727ad89c0c918
Marzuk Rashid <marzukr@gmail.com>
2019-02-11 23:23:23 -0600

    ready for deployment 1.0

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    apps : [{
+        name: 'zns',
+        script: 'app.js',
+
+        // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '2G',
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production'
+        }
+    }],
+
+    deploy : {
+        production : {
+            user : 'marzukr', //SSH user
+            host : '159.203.123.64', //SSH host
+
+            //Git stuff
+            ref  : 'origin/zuk',
+            repo : 'git@github.com:marzukr/bh-inventory.git',
+            
+            //ZNS
+            path : '/home/marzukr/zns',
+            'post-deploy' : 'npm install'
+        }
+    }
+};
