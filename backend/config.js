476f823873529362ee5fa267f0d7a30a31997054
Marzuk Rashid <marzukr@gmail.com>
2019-04-10 16:04:26 -0500

    club bug fix

--- backend/config.js
+++ backend/config.js
@@ -3,5 +3,5 @@ module.exports = {
     passwordHashCost: 10,
     serverPort: 3001,
     maxTaskPartners: 2,
-    skillAuthorizations: {0: [0], 1: [2,3], 2: [-1], 3: [1, 4, -3]}
+    skillAuthorizations: {0: [0], 1: [0,2,3], 2: [0,2,3,-1], 3: [0,2,3,-1,1, 4, -3]}
 };
